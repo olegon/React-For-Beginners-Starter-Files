04cca3ddb277471713789fdb48fec6c2ff9081bf
olegon <olegon.main@gmail.com>
2017-06-16 10:25:58 -0300

    Importando CSS com Webpack.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './css/style.css';
 import StorePicker from './components/StorePicker'
 
 ReactDOM.render(<StorePicker />, document.querySelector('#main'));
