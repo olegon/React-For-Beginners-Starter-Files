8e006d782f5b6308e1986881e91f5a9a796025e7
olegon <olegon.main@gmail.com>
2017-06-16 11:17:41 -0300

    React stateless function!

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
-export default class Header extends React.Component {
-    render() {
-        const { tagline } = this.props;
 
-        return (
-            <header className="top">
-                <h1>
-                    Catch
-                    <span className="ofThe">
-                        <span className="of">of</span>
-                        <span className="the">the</span>
-                    </span>
-                    Day
-                </h1>
-                <h3 className="tagline"><span>{ tagline }</span></h3>
-            </header>
-        );
-    }
+export default function Header (props) {
+     const { tagline } = props;
+
+    return (
+        <header className="top">
+            <h1>
+                Catch
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                Day
+            </h1>
+            <h3 className="tagline"><span>{ tagline }</span></h3>
+        </header>
+    );
 }
