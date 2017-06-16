9103857472e00e1e35446e84387eace72e7a75ac
olegon <olegon.main@gmail.com>
2017-06-16 10:23:14 -0300

    Um pouco sobre JSX.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,12 @@ import React from 'react';
 export default class StorePicker extends React.Component {
     render() {
         return (
-            <div>opa</div>
+            <form className="store-selector">
+                { /* Isso é um comentário. */ }
+                <h2>Please Enter a Store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store</button>
+            </form>
         );
     }
 }
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import StorePicker from './components/StorePicker'
 
-render(<StorePicker />, document.querySelector('#main'));
+ReactDOM.render(<StorePicker />, document.querySelector('#main'));
 
