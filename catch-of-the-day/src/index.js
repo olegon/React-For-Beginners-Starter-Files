600a9d7d7401dd047041a027683a7a5c2892b2d6
olegon <olegon.main@gmail.com>
2017-06-16 11:31:16 -0300

    ReactRouter

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 import './css/style.css';
 import StorePicker from './components/StorePicker'
 import App from './components/App';
+import NotFound from './components/NotFound';
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker} />
+                <Match exactly pattern="/store/:storeId" component={App} />
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+    );
+}
+
+ReactDOM.render(<Root />, document.querySelector('#main'));
 
