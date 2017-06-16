76e11d80eea00fed83a055796234b0ddb7c2db22
olegon <olegon.main@gmail.com>
2017-06-16 17:08:44 -0300

    React Refs

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 export default class StorePicker extends React.Component {
-    render() {
+
+    goToStore (e) {
+        e.preventDefault();
+        
+        console.log(this.storeInput);
+    }
+
+    render () {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={ this.goToStore.bind(this) }>
                 { /* Isso é um comentário. */ }
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
+                <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } ref={ (input) => { this.storeInput = input } } />
                 <button type="submit">Visit Store</button>
             </form>
         );
