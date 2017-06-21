dfe6ea9884c83793e139c49c21dc86e6aae03484
olegon <olegon.main@gmail.com>
2017-06-21 17:06:51 -0300

    Renderizando HTML com JSX.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
 
 export default class App extends React.Component {
     constructor () {
@@ -35,6 +36,9 @@ export default class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="list-of-fishes">
+                        { Object.keys(this.state.fishes).map(key => <Fish key={ key } details={ this.state.fishes[key] } />) }
+                    </ul>
                 </div>
                 <Order />
                 <Inventory addFish={ this.addFish } loadFishes={ this.loadFishes } />
