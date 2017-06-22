6b0afeb9d6ce6ee3132a43080b5b63d530d0cb95
olegon <olegon.main@gmail.com>
2017-06-22 14:45:05 -0300

    Uso de métodos "renders" auxiliares.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -42,15 +42,17 @@ export default class App extends React.Component {
     }
 
     render () {
+        const { fishes, order } = this.state;
+
         return (
             <div className="catch-of-the-day">
-                <div className="menu">
+               <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="list-of-fishes">
-                        { Object.keys(this.state.fishes).map(key => <Fish key={ key } index={ key } addToOrder={ this.addToOrder } details={ this.state.fishes[key] } />) }
+                        { Object.keys(fishes).map(key => <Fish key={ key } index={ key } addToOrder={ this.addToOrder } details={ fishes[key] } />) }
                     </ul>
-                </div>
-                <Order />
+                </div> 
+                <Order fishes={ fishes } order={ order } />
                 <Inventory addFish={ this.addFish } loadFishes={ this.loadFishes } />
             </div>
         );
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,61 @@
 import React from 'react';
 
+import { formatPrice } from '../helpers.js'
+
 export default class Order extends React.Component {
+    totalPrice() {
+        const { fishes, order } = this.props;
+
+        const total = Object.keys(order).reduce((total, key) => {
+            const count = order[key];
+            const fish = fishes[key];
+
+            if (fish.status === 'available') {
+                return total + count * fish.price;   
+            }
+            else {
+                return total;
+            }            
+        }, 0);
+
+        return total;
+    }
+    
+    renderOrder (key) {
+        const { fishes, order } = this.props;
+        const count = order[key];
+        const fish = fishes[key];
+        const fishName = fish ? fish.name : 'fish';
+
+        if (!fish || fish.status !== 'available') {
+            return (
+                <li key={ key }>Sorry, { fishName } no longer available!</li>
+            );
+        }
+        else {
+            return (
+                <li key={ key }>
+                    <span>{count}lbs {fishName}</span>
+                    <span className="price">{ formatPrice(count * fish.price) }</span>
+                </li>
+            );
+        }
+    }
+
     render() {
+        const { order } = this.props;
+        const totalPrice = this.totalPrice();
+
         return (
-            <div>Order</div>
+            <div className="order-wrap">
+                <h2>Order</h2>
+                <ul className="order">
+                    { Object.keys(order).map(key => this.renderOrder(key)) }
+                    <li className="total">
+                        <strong>Total: </strong> { formatPrice(totalPrice) }
+                    </li>
+                </ul>                
+            </div>
         );
     }
 }
