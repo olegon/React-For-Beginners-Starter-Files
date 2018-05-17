2960782cefc659753d5442624476dfc0cca6b0d0
olegon <olegon.main@gmail.com>
2018-05-17 16:40:27 -0300

    Adding the first React component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+ReactDOM.render(<StorePicker />, document.querySelector('#main'));
