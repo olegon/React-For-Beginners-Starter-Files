7261bae45bcec54f4fc99e4ce843c315028b6646
olegon <olegon.main@gmail.com>
2018-05-17 16:57:59 -0300

    Adding basic project components.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 class StorePicker extends Component {
+    constructor () {
+        super();
+
+        this.state = {
+            storeName: ''
+        };
+    }
+
     render () {
+        const { storeName } = this.state;
+
         return (
             <form className="store-selector">
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input type="text" required placeholder="Store Name" value={storeName} />
                 <button type="submit">Visit Store</button>
             </form>
         );
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import StorePicker from './components/StorePicker';
+import './css/style.css';
 
-ReactDOM.render(<StorePicker />, document.querySelector('#main'));
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.querySelector('#main'));
