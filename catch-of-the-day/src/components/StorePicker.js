ebd61d563181c730355ddefa276ed4fa83a4720a
olegon <olegon.main@gmail.com>
2018-05-18 15:14:44 -0300

    Get input values (React Refs)

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
-    render () {
+    storeNameRef = React.createRef();
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        const storeName = this.storeNameRef.current.value;
+
+        const { history } = this.props;
+
+        history.push(`/store/${storeName}`);
+    }
+
+    render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.handleSubmit}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input type="text" required placeholder="Store Name" ref={this.storeNameRef} defaultValue={getFunName()} />
                 <button type="submit">Visit Store</button>
             </form>
         );
